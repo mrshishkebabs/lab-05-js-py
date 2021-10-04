6fc47105a88c0f4d5e00393aea50f403ab78f05c
Ken Park <xkenxparkx@gmail.com>
2021-10-04 16:11:46 -0400

    finished greeting in both py and js

---
+++ greetings.py
@@ -0,0 +1,8 @@
+﻿username = input("Hey what's your name? ");
+	# username is a symbol representing a placeholder in memory (variable)
+	# input() is a FUNCTION that collects user input
+	# the output of the input() function is ASSIGNED to the variable username
+print("Hi " + str(username) + "!") 
+	# print() is a PRINT function in Python
+	# str() is a function in Python that converts its input into a STRING!
+	# + is a symvol for string concatenation!
